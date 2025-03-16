3ef9c7a4b2ea842740d1632acefdb809b245ccc9
PedroFerreiraTI <59745153+Pedrovsdf@users.noreply.github.com>
2025-03-16 14:58:36 -0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,25 @@
 <?php
+
 use App\Http\Controllers\ContatoController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
+// ROTAS PÚBLICAS
 Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
-Route::middleware('auth:sanctum')->get('/me', [AuthController::class, 'me']);
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+
+// ROTAS PROTEGIDAS
 Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/me', [AuthController::class, 'me']);
+
+    // CRUD de Contatos
     Route::apiResource('contatos', ContatoController::class);
+
+    // Filtragem de Contatos
+    Route::get('/contatos/filtrar', [ContatoController::class, 'filtrar']);
     Route::get('/contatos/filtrar/{status}', [ContatoController::class, 'index']);
 });
-Route::middleware('auth:sanctum')->post('/contatos', [ContatoController::class, 'store']);
-Route::middleware('auth:sanctum')->get('/contatos/filtrar', [ContatoController::class, 'filtrar']);
 
+// routes/api.php
+Route::middleware('auth:sanctum')->get('/contatos/exportar', [ContatoController::class, 'exportarContatos']);
